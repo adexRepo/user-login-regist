f82ac445ff77c708eaff32623325412b6703bd27
Adek Kristiyanto <aeitiao@gmail.com>
2022-12-10 21:19:03 +0700

    initial

---
+++ src/main/java/adexRepo/userloginregist/UserLoginRegistApplication.java
@@ -0,0 +1,13 @@
+package adexRepo.userloginregist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegistApplication.class, args);
+	}
+
+}
